9f159c8eb9e6775894f49216a705a1141bdd44a3
koma <komazhang@foxmail.com>
2017-04-20 14:50:50 +0800

    支持批量从文件夹导入

--- external/import.php
+++ external/import.php
@@ -3,33 +3,35 @@ if (!defined('XHGUI_ROOT_DIR')) {
     require dirname(dirname(__FILE__)) . '/src/bootstrap.php';
 }
 
+$options = getopt('p:');
 
-$options = getopt('f:');
-
-if (!isset($options['f'])) {
-    throw new InvalidArgumentException('You should define a file to be loaded');
+if (!isset($options['p'])) {
+    throw new InvalidArgumentException('You should define a path to be loaded');
 } else {
-    $file = $options['f'];
+    $path = $options['p'];
 }
 
-if (!is_readable($file)) {
+if (!is_readable($path)) {
     throw new InvalidArgumentException($file.' isn\'t readable');
 }
 
-$fp = fopen($file, 'r');
-if (!$fp) {
-    throw new RuntimeException('Can\'t open '.$file);
-}
-
-$container = Xhgui_ServiceContainer::instance();
-$saver = $container['saverMongo'];
-
-
-while (!feof($fp)) {
-    $line = fgets($fp);
-    $data = json_decode($line, true);
-    if ($data) {
-        $saver->save($data);
+$objects = scandir($path);
+foreach ( $objects as $file ) {
+    $file = $path.'/'.$file;
+    $fp = fopen($file, 'r');
+    if (!$fp) {
+        throw new RuntimeException('Can\'t open '.$file);
     }
+
+    $container = Xhgui_ServiceContainer::instance();
+    $saver = $container['saverMongo'];
+
+    while (!feof($fp)) {
+        $line = fgets($fp);
+        $data = json_decode($line, true);
+        if ($data) {
+            $saver->save($data);
+        }
+    }
+    fclose($fp);
 }
-fclose($fp);
